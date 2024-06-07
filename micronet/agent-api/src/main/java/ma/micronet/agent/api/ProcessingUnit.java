d376e215ae61888a50c7436c68fae14063fee2ad
asebban <asebban@omnidata.ma>
2024-06-07 19:27:01 +0100

    > Added ProcessingUnit class that implements IProcessingUnit interface. In this class, added callAgent method to help agent processing units that extend it in calling other agents

---
+++ micronet/agent-api/src/main/java/ma/micronet/agent/api/ProcessingUnit.java
@@ -0,0 +1,39 @@
+package ma.micronet.agent.api;
+
+import ma.micronet.commons.Message;
+import ma.micronet.commons.MicroNetException;
+import ma.micronet.router.api.Router;
+import ma.micronet.router.api.RouterConnection;
+
+import java.io.IOException;
+
+public abstract class ProcessingUnit implements IProcessingUnit {
+
+    private Agent agent;
+
+    public abstract Message execute(Message message) throws MicroNetException;
+
+    public abstract String registerRelativePath();
+
+    public Message callAgent(Message request, String command, String path, String payload ) throws MicroNetException, IOException {
+
+        Message requestToAgent = Message.copy(request);
+        Router router = new Router();
+        RouterConnection routerConnection = router.createConnection();
+        
+        requestToAgent.setCommand(command);
+        requestToAgent.setDirection(Message.REQUEST);
+        requestToAgent.setSenderAdressable(this.agent);
+        requestToAgent.setSenderType(Message.AGENT_TYPE);
+        requestToAgent.setTargetAdressable(this.agent);
+        requestToAgent.setTargetType(Message.AGENT_TYPE);
+
+        requestToAgent.setPath(path);
+        requestToAgent.setPayLoad(payload);
+
+        routerConnection.connect();
+        Message response = routerConnection.sendSync(requestToAgent);
+        return response;
+    }
+
+}
